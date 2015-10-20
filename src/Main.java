65e121c45c2174ca2a5d0195ef0e9f0050aee8b1
lucas <luben93@gmail.com>
2015-10-20 20:19:52 +0200

    init

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String ip="localhost";
+        Scanner s = new Scanner(System.in);
+        try {
+            AudioStreamUDP a = new AudioStreamUDP();
+            System.out.println("my loacl port is: " +a.getLocalPort());
+            System.out.println("feed me port:");
+            String in =s.nextLine();
+            a.connectTo(InetAddress.getByName(ip), Integer.parseInt(in));
+            a.startStreaming();
+
+            System.out.println("stopping stream");
+            a.stopStreaming();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
